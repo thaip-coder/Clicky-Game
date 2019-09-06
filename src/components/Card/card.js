9b8480a9ab387d51f249c2be3a4c5e481bd90964
Thai Phan <thaiphan90@yahoo.com>
2019-09-06 17:09:28 -0500

    why wont bootstrap work

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './Card.css';
 
 const Card = props => (
-        <div 
+    <div 
         classname ="card" 
         value={props.id} 
-        onClick={() => props.clickEvent(props.id)}>
-            <div className="img-container">
-                <img alt={props.name} src={props.image}/>
-            </div>
+        onClick={() => props.clickEvent(props.id)}
+    >
+        <div className="img-container">
+            <img alt={props.name} src={props.image}/>
         </div>
+    </div>
 );
 
 export default Card;
