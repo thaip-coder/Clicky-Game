f792f3a9f7a04919e4e7619355163f82a6c426b0
Thai Phan <thaiphan90@yahoo.com>
2019-09-07 16:46:31 -0500

    react css is confusing

--- src/components/Wrapper/wrapper.js
+++ src/components/Wrapper/wrapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Wrapper.css';
 
-const Wrapper = props => <div className="wrapper container">{props.children}</div>
+const Wrapper = props => <div className="wrapper">{props.children}</div>
 
 export default Wrapper;
