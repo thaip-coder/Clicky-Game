248741c847d53426ce7b5317c4061325680383dd
Thai Phan <thaiphan90@yahoo.com>
2019-09-07 16:15:50 -0500

    updated card spacing

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './Nav.css';
 
 const Nav = props => (
-    <div className='container'>
+    
     <nav className='navbar navbar-light bg-light'>
         <ul className='navbar navbar-light bg-light'>
             <li className='navbar navbar-light bg-light'>
-                <a className='navbar-brand' href='/Clicky-Game/'>{props.title}</a>
+                <a className='navbar-brand' href='/Clicky-Game/' style={{float: "left"}}>{props.title}</a>
             </li>
             <li className='navbar navbar-light bg-light' id='rw'>{props.rightWrong}</li>
             <li className='navbar navbar-light bg-light' id='current'>Current Score: {props.score}</li>
             <li className='navbar navbar-light bg-light' id='top'>High Score: {props.highScore}</li>
         </ul>
     </nav>
-    </div>
+    
 );
 
 export default Nav;
