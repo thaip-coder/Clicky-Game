9e9b36f906d2de8114000ba16d9fbbcdbcd9060e
Thai Phan <thaiphan90@yahoo.com>
2019-09-07 17:25:55 -0500

    Updates

---
+++ precache-manifest.8a3cd31182164b76c06a36c6ee51adf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50e4d20ee26d95370e3e0e6c62f5f65",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "a565f3ab3bc0444f0dde",
+    "url": "/Clicky-Game/static/css/main.e44e9c59.chunk.css"
+  },
+  {
+    "revision": "860f036b0afabf2eb890",
+    "url": "/Clicky-Game/static/js/2.bd0dc2ea.chunk.js"
+  },
+  {
+    "revision": "a565f3ab3bc0444f0dde",
+    "url": "/Clicky-Game/static/js/main.8865a688.chunk.js"
+  },
+  {
+    "revision": "a34a2661cf4f659bac3b",
+    "url": "/Clicky-Game/static/js/runtime~main.e9eafbd0.js"
+  }
+]);
